cc49a89d143cae66e43cdbcd29f8d4e84b086311
Subash1237 <rimalsubash77@gmail.com>
2023-09-22 20:57:08 +0545

    sdf

--- index.js
+++
@@ -1,5 +0,0 @@
-
-// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
-
-var names="John",age="23",sex="Male",from="External script.."
-console.log(names,age,sex,from)
