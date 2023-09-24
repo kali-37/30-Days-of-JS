d95441c8dcce21413f9c1919c739d452f9fdca5d
Subash1237 <rimalsubash77@gmail.com>
2023-09-24 09:19:39 +0545

    keeping .se ,se

--- DAY1/DAY2/Day2.js
+++ DAY1/DAY2/Day2.js
@@ -77,3 +77,58 @@ console.log(stringerer.split(', ')) // returns array of characters  a b c d e f
 let forefathers =" we came here to learn javascript from fore fathers of javascript"
 console.log(forefathers.includes('javascript')) // returns true if javascript is present in the string
 console.log(forefathers.includes('python')) // returns false if python is not present in the string
+
+console.log(forefathers.indexOf('javascript')) // returns index of javascript in the string
+console.log(forefathers.indexOf('python')) // returns -1 if python is not present in the string
+
+
+
+
+// Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+console.log(forefathers.indexOf('a')) // to display last occurence of a in the string we can use lastIndexOf
+console.log(forefathers.lastIndexOf('a'))
+
+
+let sentence="You cannot end a sentence with because because because is a conjunction"
+console.log(sentence.lastIndexOf('because'))
+console.log("------------------------------------------------------------------------------")
+console.log(sentence.matchAll("because")) // returns index of all occurence of because in the string
+
+
+
+// Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+
+
+
+console.log(forefathers.replace('javascript','python')) // replaces javascript with python in the string
+console.log(forefathers.replace(/javascript/g,'python')) // replaces all javascript with python in the string
+
+console.log(forefathers.charAt(2)) // returns character at index 0
+
+console.log(forefathers.indexOf('p')) // returns index of javascript in the string
+
+
+console.log(forefathers.charCodeAt('2')) // returns unicode of character at index 0
+
+
+let strings = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log(strings.match('love')) // returns first occurence of love in the string
+console.log(strings.match(/love/g)) // returns all occurence of love in the string
+console.log(strings.repeat(2))
+
+
+let num = '10a12' // string
+console.log(typeof num) // this will print string 
+let numInt = parseInt(num) // returns integer value of num as string so output is 10 and 12 is ignored because it is not a number
+console.log(numInt) // 10
+console.log(typeof numInt) // number
+
+
+
+
+const sentences = "You cannot end a sentence with because because because is a conjunction"
+const startingIndex = sentences.indexOf("b")
+const endingIndex = sentences.lastIndexOf("e") + 1
+const phrase = sentences.substr(startingIndex, endingIndex - startingIndex)
+
+console.log(phrase); // "because because because"0
